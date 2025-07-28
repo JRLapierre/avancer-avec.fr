cbfe1507d6d1acba1ce611f9d3a4975a46db003c
Lapierre JeanRémi <jeanremi.lapierre@gmail.com>
2025-07-28 16:54:07 +0200

    informations footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,6 @@ const App = () => {
   return (
     <div className="page">
       <h1>Site en construction</h1>
-      <div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab.</div>
-      </div>
     </div>
   )
 }
--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -1,11 +1,28 @@
-import styles from './styles.module.css';
+import './styles.module.css';
 
 const Footer = () => {
     return (
         <footer>
-            <div>colonne 1</div>
-            <div>colonne 2</div>
-            <div>colonne 3</div>
+            {/* Column 1 */}
+            <div>
+                <h3>Où ?</h3>
+                <p>A domicile, via visioconférence ou en extérieur</p>
+                <p>Basée à Hénin-Beaumont</p>
+            </div>
+            {/* Column 2 */}
+
+            <div>
+                <h3>Horaires</h3>
+                <p>Lundi 9h - 12h</p>
+                <p>Mardi au Vendredi 9h - 20h</p>
+                <p>Samedi matin 9h - 13h</p>
+            </div>
+            {/* Column 3 */}
+            <div>
+                <h3>Contact</h3>
+                <p>06.37.24.35.40</p>
+                <p>clairelise@avancer-avec.fr</p>
+            </div>
         </footer>
     )
 
