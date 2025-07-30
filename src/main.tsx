91644c26a3d5e02761e111eb346a86a549bc3bab
Lapierre JeanRémi <jeanremi.lapierre@gmail.com>
2025-07-30 16:18:38 +0200

    attempt fix navigation 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import Layout from './Layout/index.tsx';
 import NoPage from './NoPage/index.tsx';
@@ -11,7 +11,7 @@ import Blog from './Blog/index.tsx';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
@@ -20,6 +20,6 @@ createRoot(document.getElementById('root')!).render(
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
-    </Router>
+    </HashRouter>
   </StrictMode>,
 )
