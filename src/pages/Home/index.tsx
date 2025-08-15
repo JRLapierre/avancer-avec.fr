2a94ccde4d18e965afaf6a65ab386166233ea355
Lapierre JeanRémi <jeanremi.lapierre@gmail.com>
2025-08-15 11:39:54 +0200

    homepage hook

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import welcomeImage from '../../assets/page/Home/chemin-texte.png'
+//import welcomeImage from '../../assets/page/Home/chemin-texte.png' //TODO delete useless image
+import styles from './styles.module.css'
 
 const Home: React.FC = () => {
     return (
         <>
-            <h1>Accueil</h1>
-            <img src={welcomeImage} alt="bienvenue" />
+        <div className={styles.hook}>
+            <div className={styles.bloc1}>
+                <h1>Vous avez envie d’avancer ?</h1>
+                <h5>De dépasser les frustrations, blocages, craintes ?</h5>
+            </div>
+            <div className={styles.bloc2}>
+                <h2>Je vous accompagne !</h2>
+            </div>
+            <div className={styles.bloc3}>
+                <h3>Claire-Lise, coach certifiée à Hénin-Beaumont</h3>
+            </div>
+        </div>
+        <div>
+            {/* TODO photo and comment */}
+        </div>
+        <div>
+            {/* TODO Buttons towards "ce que je vous propose" and "mon parcours, ma vision" */}
+        </div>
         </>
     )
 }
