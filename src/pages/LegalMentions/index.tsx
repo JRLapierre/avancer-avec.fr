f8c6fcb861011110d8837e8fc1e524d6d4c660de
Lapierre JeanRémi <jeanremi.lapierre@gmail.com>
2025-09-05 09:06:21 +0200

    Legal mentions

---
+++ src/pages/LegalMentions/index.tsx
@@ -0,0 +1,66 @@
+const LegalMentions = () => {
+    return (
+        <div>
+            <p><strong>Mentions légales et Politique de confidentialité</strong></p>
+
+            <p><u>Données personnelles</u>&nbsp;: ce qui permet de vous identifier tels que nom, prénom, adresse mail, téléphone, RIB si virement. Certaines de ces données peuvent vous être demandées lors de votre utilisation du site ou liées à la mission d'accompagnement proposée. De simples visites sur le site ne nécessitent ni inscription ni identification préalable.</p>
+
+            <p>Ces données sont utilisées pour&nbsp;:</p>
+
+            <p>- Exécuter les missions d'accompagnement que vous demandez, facturer les contrats, gérer et suivre la relation commerciale avec vous. Le traitement de vos données est fondé sur l'exécution du contrat&nbsp;;</p>
+
+            <p>- Réaliser des opérations de prospection commerciale. Ce traitement est fondé sur notre intérêt légitime ;</p>
+
+            <p>- Gérer la comptabilité et répondre à nos obligations fiscales. Ce traitement est fondé sur une obligation légale&nbsp;;</p>
+
+            <p>-Répondre à vos demandes d'information et de prise de contact. Ce traitement est basé sur votre consentement&nbsp;;</p>
+
+            <p>-Gérer notre site (analyses, études, sondages) à l'interne. Ce traitement est fondé sur notre intérêt légitime.</p>
+
+            <p><u>Responsable du traitement</u>&nbsp;: Claire-Lise Lapierre, <em>avancer-avec</em></p>
+
+            <p>Téléphone +&nbsp;33 (0)6 37 24 35 40.     mail&nbsp;: <a href="mailto:claireliselapierre@gmail.com">claireliselapierre@gmail.com</a></p>
+
+            <p><u>Conservation des données</u>&nbsp;: les données utilisées aux fins d'exécution des missions que vous nous confiez sont conservées pendant toute la durée de la relation contractuelle, puis archivées. L'archivage implique que vos données ne seront plus consultables en ligne mais seront extraites et conservées sur un support autonome et sécurisé.</p>
+
+            <p><u>Vos droits sur vos données</u>&nbsp;:  </p>
+
+            <p>- Droit à l'information: vous avez le droit d'être informé du traitement de vos données personnelles et vous pouvez nous contacter à l'adresse&nbsp;: <a href="mailto:claireliselapierre@gmail.com">claireliselapierre@gmail.com</a> </p>
+
+            <p>-Droit d'accès: vous avez le droit d'accéder à tout moment à l'ensemble de vos données personnelles.   </p>
+
+            <p>-Droit de rectification: vous avez le droit de rectifier à tout moment vos données personnelles inexactes, incomplètes ou obsolètes.</p>
+
+            <p>-Un droit d'effacement: dans certains cas, vous avez le droit d'obtenir l'effacement de vos données. Cependant, ceci n'est pas un droit absolu et nous pouvons, pour des raisons légales ou légitimes, conserver ces données.</p>
+
+            <p>-Droit à la limitation: vous avez le droit d'obtenir la limitation du traitement de vos données à caractère personnel dans certains cas définis à l'art.18 du RGPD.</p>
+
+            <p>-Droit d'introduire une réclamation: auprès de la CNIL en France, si vous considérez que le traitement de vos données personnelles constitue une violation des textes applicables.</p>
+
+            <p>-Droit d'opposition: vous avez le droit de vous opposer au traitement de vos données personnelles. Notez toutefois que nous pourrons maintenir leur traitement malgré cette opposition, pour des motifs légitimes ou la défense de droits en justice.   </p>
+
+            <p>-Le droit de retirer votre consentement à tout moment: vous pouvez retirer votre consentement au traitement de vos Données lorsque le traitement est basé sur votre consentement. Le retrait du consentement ne compromet pas la licéité du traitement fondé sur le consentement effectué avant ce retrait.</p>
+
+            <p>-Le droit de donner des directives concernant le sort de vos données après votre décès: vous avez le droit de nous donner des directives concernant l'utilisation de vos Données après votre décès.</p>
+
+            <p>Vous pouvez exercer les droits ci-dessus en nous écrivant à&nbsp;: <a href="mailto:claireliselapierre@gmail.com">claireliselapierre@gmail.com</a></p>
+
+            <p>Le site internet Claire-Lise_coach_pour_avancer, propose des articles de blog.</p>
+
+            <p>Il est strictement interdit de copier le contenu de ce blog et de le publier sur un autre blog sans l'accord de l'auteur. Le propriétaire peut donc recevoir une compensation s'il y a transgression de cette clause.</p>
+
+            <p>L'adresse email de l'Utilisateur ne sera jamais vendue ou partagée avec qui que ce soit.</p>
+
+            <p><strong>Modifications et application de la politique de confidentialité</strong></p>
+
+            <p>Nous nous réservons le droit, à notre seule initiative, de modifier à tout moment la présente charte, en totalité ou en partie. Ces modifications entreront en vigueur à compter de la publication de la nouvelle charte. Nous vous en informerons au préalable par tout moyen utile.</p>
+
+            <p>Mis à jour le 16 Juillet 2025</p>
+
+            <p>      </p>
+
+        </div>
+    )
+}
+
+export default LegalMentions
